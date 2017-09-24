906dd11ad5f7f90adf8360009c443926cbe5a66e
sousvin <vinicius.sousa.brito@gmail.com>
2017-09-23 23:45:12 -0300

    adicionado exercicio 02

---
+++ lista01/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)    {    
+        int x, y, soma;
+
+	scanf("%d", &x);
+	scanf("%d", &y);
+	soma= x+y;
+	
+	printf("O resultado da soma é %d \n", soma);
+	return 0;
+
+}
+
+	
