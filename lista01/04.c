0825205b4d320e426b73ec003f3b86b50221a217
sousvin <vinicius.sousa.brito@gmail.com>
2017-09-28 00:36:43 -0300

    adicionando exercicio 04 e 05

---
+++ lista01/04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)    {    
+        int x, y, mult;
+
+	scanf("%d", &x);
+	scanf("%d", &y);
+	mult= x*y;
+	
+	printf("O resultado da multiplicação é %d \n", mult);
+	return 0;
+
+}
+
+	
---
+++ lista01/05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)    {    
+        int x, y, div;
+
+	scanf("%d", &x);
+	scanf("%d", &y);
+	div= x/y;
+	
+	printf("O resultado da divisão é %d \n", div);
+	return 0;
+
+}
+
+	
