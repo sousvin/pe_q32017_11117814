e09581401b0b7b8b339be9d72d61ebe18a50ad11
sousvin <vinicius.sousa.brito@gmail.com>
2017-09-27 23:07:38 -0300

    adicionado exercicio 03

---
+++ lista01/03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)    {    
+        int x, y, sub;
+
+	scanf("%d", &x);
+	scanf("%d", &y);
+	sub= x-y;
+	
+	printf("O resultado da subtração é %d \n", sub);
+	return 0;
+
+}
+
+	
